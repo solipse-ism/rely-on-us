48c9d5a1e5a36c982c8d88e17bead6f4b62fcb11
solipse-ism <totallystudyign@gmail.com>
2023-10-24 22:25:28 +0700

    Test Deployment 07

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './assets/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router basename={"./"}>
+    <Router basename={"/"}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
