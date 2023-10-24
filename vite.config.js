c258c63053ab0a69f6ae8c22142730405d3c3a2a
solipse-ism <totallystudyign@gmail.com>
2023-10-24 21:43:04 +0700

    Test Deployment 3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/rely-on-us"
+  base: "/rely-on-us/",
+  plugins: [react()]
 })
