768f242d5bafd4889ac5810a7751c95e0b4466a2
solipse-ism <totallystudyign@gmail.com>
2023-10-28 23:56:43 +0700

    Fix build problem

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 export { default as Home } from './home/Home';
 export { default as Community } from './community/Community';
 export { default as Event } from './event/Event';
-export { default as Inspiring } from './event/Inspiring';
+export { default as Inspiring } from '/src/pages/event/Inspiring';
 export { default as Dashboard } from './dashboard/Dashboard';
 export { default as Error404 } from './error-page/NotFound';
